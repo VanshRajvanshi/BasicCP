863bc2aea4245213f90957e456706245e7dbe62b
Vansh Rajvanshi <168770432+VanshRajvanshi@users.noreply.github.com>
2025-08-18 12:13:18 +0530

    Add files via upload

---
+++ CP PYTHON/Loops/POwer.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+i=1
+p=1
+while i<=b:
+    p=p*a
+    i+=1
+print("Power:", p)
---
+++ CP PYTHON/Loops/SumOfEven.py
@@ -0,0 +1,7 @@
+A=int(input("Enter the number"))
+sum=0
+i=2
+while i <= A:
+    sum += i
+    i += 2
+print("Sum of even numbers:", sum)
